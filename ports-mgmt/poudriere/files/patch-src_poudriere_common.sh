a73e15ded0f886914f48d06af343c68fbf5f472b
Emil Smolenski <moleskin-smile@nereis.pl>
2013-10-14 13:20:36 +0200

    Update devfs patch for ports-mgmt/poudriere

--- ports-mgmt/poudriere/files/patch-src_poudriere_common.sh
+++ ports-mgmt/poudriere/files/patch-src_poudriere_common.sh
@@ -1,6 +1,6 @@
---- src/share/poudriere/common.sh.orig	2013-06-10 14:51:21.758617306 +0200
-+++ src/share/poudriere/common.sh	2013-06-10 14:53:02.193812885 +0200
-@@ -740,10 +740,12 @@
+--- src/share/poudriere/common.sh.orig	2013-10-10 04:20:34.000000000 +0200
++++ src/share/poudriere/common.sh	2013-10-14 13:19:14.930072700 +0200
+@@ -765,10 +765,12 @@
  
  	# ref jail only needs devfs
  	mount -t devfs devfs ${mnt}/dev
@@ -17,3 +17,12 @@
  	if [ "${mnt##*/}" != "ref" ]; then
  		[ ${JAILED} -eq 0 ] && mount -t fdescfs fdesc ${mnt}/dev/fd
  		mount -t procfs proc ${mnt}/proc
+@@ -1906,7 +1908,7 @@
+ 	buildlog_start ${portdir}
+ 
+ 	# Ensure /dev/null exists (kern/139014)
+-	devfs -m ${mnt}/dev rule apply path null unhide
++	[ ${JAILED} -eq 0 ] && devfs -m ${mnt}/dev rule apply path null unhide
+ 
+ 	if [ -n "${ignore}" ]; then
+ 		msg "Ignoring ${port}: ${ignore}"
