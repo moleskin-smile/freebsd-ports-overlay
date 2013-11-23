baedf569acff7875e91aabd02aba19ccd319bbb5
Emil Smolenski <moleskin-smile@nereis.pl>
2013-11-24 00:25:45 +0100

    Add devel/py-qt4/bsd.pyqt.mk file

---
+++ devel/py-qt4/bsd.pyqt.mk
@@ -0,0 +1,25 @@
+# bsd.pyqt.mk - Global definitions for PyQt related ports
+#
+# $FreeBSD: devel/py-qt4/bsd.pyqt.mk 330463 2013-10-15 21:52:33Z rakuco $
+#
+
+PYQT_MAINTAINER=	kde@FreeBSD.org
+
+MASTER_SITE_RIVERBANK=	http://www.riverbankcomputing.com/static/Downloads/%SUBDIR%/
+
+MASTER_SITES_SIP=	SF/pyqt/sip/sip-${PORTVERSION} \
+			${MASTER_SITE_GENTOO:S,%SUBDIR%,distfiles,}
+MASTER_SITES_PYQT4=	SF/pyqt/PyQt4/PyQt-${PORTVERSION} \
+			${MASTER_SITE_GENTOO:S,%SUBDIR%,distfiles,}
+MASTER_SITES_QSCI2=	SF/pyqt/QScintilla2/QScintilla-${PORTVERSION} \
+			${MASTER_SITE_GENTOO:S,%SUBDIR%,distfiles,}
+
+SIP_VERSION=		4.15.2
+PYQT4_VERSION=		4.10.3
+QSCI2_VERSION=		2.7.2
+
+SIP_DISTNAME=		sip-${SIP_VERSION}
+PYQT4_DISTNAME=		PyQt-x11-gpl-${PYQT4_VERSION}
+PYQT4_DISTINFO_FILE=	${.CURDIR}/../../devel/py-qt4/distinfo
+QSCI2_DISTNAME=		QScintilla-gpl-${QSCI2_VERSION}
+QSCI2_DISTINFO_FILE=	${.CURDIR}/../../devel/qscintilla2/distinfo
