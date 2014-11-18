dbe19accb95097e7bcce4a6e7caf31ea98a11c07
Emil Smolenski <moleskin-smile@nereis.pl>
2014-11-18 09:18:23 +0100

    Update devel/rubygem-pkg-config

--- devel/rubygem-pkg-config/files/extra-patch-lib-pkg-config.rb
+++ devel/rubygem-pkg-config/files/extra-patch-lib-pkg-config.rb
@@ -1,11 +1,14 @@
---- lib/pkg-config.rb.orig	2014-06-09 13:40:30.635922000 +0200
-+++ lib/pkg-config.rb	2014-06-09 14:43:43.429919976 +0200
-@@ -90,10 +90,10 @@
+--- lib/pkg-config.rb.orig	2014-11-18 09:09:37.045919000 +0100
++++ lib/pkg-config.rb	2014-11-18 09:16:01.461138370 +0100
+@@ -91,13 +91,13 @@
+           return nil
+         end
+         begin
+-          extern "const char *dln_find_exe(const char *, const char *)"
++          extern "const char *dln_find_exe_r(const char *, const char *, char *, size_t)"
          rescue DL::DLError
            return nil
          end
--        extern "const char *dln_find_exe(const char *, const char *)"
-+        extern "const char *dln_find_exe_r(const char *, const char *, char *, size_t)"
        end
 -      path = dln.dln_find_exe(pkg_config.to_s, nil)
 -      if path.nil? or path.size.zero?
